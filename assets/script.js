2f2dd8749fb9f477774a8759dbea2a4d71b4b441
mengyi-qian <mengyi.qian@yale.edu>
2020-10-18 03:35:11 -0400

    statement page & sleeping animation

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,27 @@
 $(document).ready(function(){
-  const title = document.querySelector('h1');
-  const playitbyear = `play it by ear`;
+
+  const livestream = document.getElementById('livestream-container');
+  const buttons = document.querySelectorAll('.button');
+  setTimeout(function(){
+    livestream.style.transform = 'translateY(-50px)';
+    console.log("ok");
+    for ( let i = 0; i < buttons.length; i++ ) {
+      buttons[i].style.display = 'block';
+    }
+  }, 2000);
+
+  var timeout;
+  document.onmousemove = function(){
+    clearTimeout(timeout);
+    console.log("timeout");
+    $('#mask').removeClass('mask-animation');
+    timeout = setTimeout(function(){
+      $('#mask').addClass('mask-animation');
+    }, 10000);
+  }
+
+  const h1 = document.querySelector('h1');
+  const title = `play it by ear`;
   const about = `
     <p>This livestream is recorded from our exhibition space.</p>
     <p>You are welcome to make a <u>reservation</u> for visit, or go to our <u>echo</u> dock, or leave us a <u>message</u>.</p>
@@ -12,15 +33,15 @@ $(document).ready(function(){
     if ( currentScroll > previousScroll ) {
       if ( currentScroll > (window.innerHeight - 50) ) {
         // console.log(currentScroll);
-        title.innerHTML = about;
-        title.className = 'about';
+        h1.innerHTML = about;
+        h1.className = 'about';
         $('h1').removeClass('fadein');
       }
     } else {
       if ( currentScroll < (window.innerHeight - 50) ) {
         // console.log(currentScroll);
-        title.innerHTML = playitbyear;
-        title.className = 'playitbyear';
+        h1.innerHTML = title;
+        h1.className = 'title';
       }
     }
     previousScroll = currentScroll;
@@ -37,6 +58,40 @@ $(document).ready(function(){
   //   button.style.top = `${positionY}px`;
   // }, 1);
 
+
+  $('#statement-right').addClass('blur');
+
+  $('#statement-left').hover(
+    function(){
+      $(this).removeClass('blur');
+      $('#statement-right').addClass('blur');
+    },function(){
+      $(this).addClass('blur');
+    }
+  );
+
+  $('#statement-right').hover(
+    function(){
+      $(this).removeClass('blur');
+      $('#statement-left').addClass('blur');
+    },function(){
+      $(this).addClass('blur');
+    }
+  );
+
+
+
+  $('.button').click(function(){
+    $('.button').css('animation-play-state','running,running,running');
+    $(this).css("animation-play-state","paused,paused,running");
+    $(this).css("pointer-events","none");
+  });
+
+  $('#statement').click(function(){
+    $('h1,#livestream-scroll').addClass('hide');
+    $('.statements').addClass('show');
+    console.log('ok');
+  });
 
 
 
